41731bf8c5d7b7cc543fd38aeff22bc03b70b15d
Bartek <bartlomiej.kacki@gmail.com>
2024-07-07 13:03:36 +0200

    Add project files.

---
+++ FavoriteFoodsDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FavoriteFoodsDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> favoriteFoods = new Dictionary<string, string>();
+            favoriteFoods["Alek"] = "hot dog";
+            favoriteFoods["Asia"] = "pizza";
+            favoriteFoods["Julek"] = "falafel";
+            favoriteFoods["Natalka"] = "spaghetti";
+
+            string name;
+
+            while((name=Console.ReadLine()) != "")
+            {
+                if (favoriteFoods.ContainsKey(name))
+                    Console.WriteLine($"Tym, co {name} lubi najbardziej, jest {favoriteFoods[name]}.");
+                else
+                    Console.WriteLine($"Nie wiem, co lubi {name}.");
+            }
+        }
+    }
+}
